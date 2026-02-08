e9aea60bfdc33c01ddc8f664378b33326219d6f1
eldemirberkay0 <eldemirberkay01@gmail.com>
2026-02-08 19:50:01 +0300

    Add player loop inserter
    Insert timer tick into player loop (Update)

---
+++ Runtime/TimerManager.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlexTimer
+{
+    public static class TimerManager
+    {
+        internal static void UpdateTimers()
+        {
+            
+        }
+    }
+}
