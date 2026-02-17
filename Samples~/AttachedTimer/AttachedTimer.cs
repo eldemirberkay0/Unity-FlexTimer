f622d3fa056dd42940613c665dd3f4449ede9242
eldemirberkay0 <eldemirberkay01@gmail.com>
2026-02-18 00:07:09 +0300

    Add attached/interval timer example
    Samples~/AttachedTimer

---
+++ Samples~/AttachedTimer/AttachedTimer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace FlexTimer.Samples
+{
+    public class AttachedTimer : MonoBehaviour
+    {
+        private float enemyHealth = 10;
+        Timer timer;
+
+        void Start()
+        {
+            // Create a timer attached to this MonoBehavior so that if MonoBehavior is destroyed, timer cancels itself.
+            timer = new Timer(2, TakeDamage, tickCount: 3, attachedTo: this);
+            timer.Start();
+        }
+
+        void TakeDamage()
+        {
+            enemyHealth -= 5;
+            Debug.Log("Enemy took damage! Current health: " + enemyHealth.ToString());
+            // Enemy dies after 2 tick, and if timer is not attached it will try to access enemy that results with an error
+            if (enemyHealth <= 0)
+            {
+                Debug.Log("Enemy died.");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
